675484d9d028a863b96b9596e09c9456cda1a03f
Joo <rudwn3166@naver.com>
2021-08-28 21:53:40 +0900

    다른 화면으로 이동하게 해주는 Intent기능을 추가했다

--- app/src/main/java/com/example/intentpractice/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+//            버튼이 눌리면 다른 화면 OtherActivity화면으로 이동해라
+//            화면 전환은 주로 Intent를 이용한다 액티비티 이동해주는 클래스
+            val myIntent=Intent(this,OtherActivity::class.java)
+
+            startActivity(myIntent)
+
+
+
+        }
+
     }
+
+
 }
