33bec8316439764ea4d54c3559d67cf443c79664
Joo <rudwn3166@naver.com>
2021-08-30 21:18:11 +0900

    닉네임 변경 화면->입력 후 완료 출력시 데이터 들고 돌아가기->메인화면에서 데이터 반영은 별도 작업 필요

--- app/src/main/java/com/example/intentpractice/EditNicNameActivity.kt
+++ app/src/main/java/com/example/intentpractice/EditNicNameActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nic_name.*
@@ -9,7 +10,22 @@ class EditNicNameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nic_name)
 
+        okBtn.setOnClickListener {
 
+//            입력한 새 닉네임이 뭔지?변수로 저장
+            val inputNewNickname = newNicknameEdt.text.toString()
+
+//            입력한 닉네임을 가지고 메인화면으로 복귀하기
+
+//            입력한 닉네임을 담아주기 위한 용도로만 사용되는 intent
+            val resultIntent = Intent()//왕복티켓임
+
+//            putExtra는 변수 resultIntent에 값을 넣어주는 기능능
+           resultIntent.putExtra("nickname",inputNewNickname)
+            setResult(RESULT_OK,resultIntent)
+            finish()
+
+        }
 
 
     }
--- app/src/main/java/com/example/intentpractice/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -9,7 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 //    멤버변수는 클래스 바로 밑에 만듬
-    val REQUEST_FOR_NICKNAME = 1005
+    val REQUEST_FOR_NICKNAME = 1005//멤버변수 선언
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
             val myIntent = Intent(this, EditNicNameActivity::class.java)
 
+//            결과를 받으러 가는 과정
            startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)//닉네임 가지러 갈때는 1005를 사용한다고 구분해줌
 //            숫자만 적으면 정확히 코드만 코드 어떤 내용인지 이해하기 어려워 멤버변수를 이용하여 가독성을 올려야함
 
